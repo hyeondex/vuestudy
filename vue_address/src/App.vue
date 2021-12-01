cf4476d11ec4386fa0ccf778c22e9330cedc056f
이수현_이모션 <soohyeon.lee@emotion.co.kr>
2021-12-01 10:03:40 +0900

    vue address add

---
+++ vue_address/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="inputText" readonly />
+    <input type="text" v-model="inputText1" readonly />
+    <input type="text" v-model="inputText2" />
+    <button type="button" @click="testText">클릭</button>
+
+    <button type="button" @click="modalOpen">클릭</button>
+    <!--    <modal v-if="modal" @closeModal=""></modal>-->
+    <modal></modal>
+  </div>
+</template>
+
+<script>
+import { getData } from "./api";
+import Modal from "./components/modal";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      inputText: "",
+      inputText1: "",
+      inputText2: "",
+      modal: "",
+    };
+  },
+  components: {
+    Modal,
+  },
+  methods: {
+    aa(a) {
+      console.log(a);
+    },
+    // 값을 가지고 옴 > 그 값을 뿌릴 메소드 제작 > 메소드를 실행시킬 이벤트 > 그 이벤트를 html 영역에 붙이기
+    testText() {
+      getData({
+        currentPage: "",
+        countPerPage: "",
+        keyword: this.inputText,
+      }).then((response) => {
+        console.log(response);
+      });
+    },
+    modalOpen() {
+      this.modal = true;
+      console.log("modal open");
+    },
+    closeModal() {
+      this.modal = false;
+      console.log("close");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_address/src/components/modal.vue
@@ -0,0 +1,90 @@
+<template id="modal">
+  <div class="modal" @click.self="$emit(closeModal)">
+    <div class="close" @click="$emit(closeModal)">&times;</div>
+    <div class="body">
+      <slot name="body" />
+      testt
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "modal",
+};
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrap {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  position: relative;
+  width: 300px;
+  margin: 0px auto;
+
+  background: #c0c0c0;
+  box-shadow: inset -1px -1px #0a0a0a, inset 1px 1px #ffffff,
+    inset -2px -2px #808080, inset 2px 2px #dfdfdf;
+  border: 1px solid #0a0a0a;
+
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header {
+  margin-top: 0;
+  color: #fff;
+  padding: 5px;
+  background: linear-gradient(90deg, #000080, #1084d0);
+  font-size: 15px;
+  margin: 0;
+}
+
+.modal-body {
+  display: flex;
+  padding: 5px;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-default-button {
+  position: absolute;
+  top: -1px;
+  right: -1px;
+  padding: 4.5px 7.5px;
+  background: #dfdfdf;
+  box-shadow: inset -1px -1px #0a0a0a, inset 1px 1px #ffffff,
+    inset -2px -2px #808080, inset 2px 2px #dfdfdf;
+  color: #0a0a0a;
+  box-sizing: border-box;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
