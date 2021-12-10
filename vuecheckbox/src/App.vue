0192c59d729dda7ccdb8bc83db6f389123e425fa
이수현_이모션 <soohyeon.lee@emotion.co.kr>
2021-12-10 12:07:30 +0900

    first commit

--- vuecheckbox/src/App.vue
+++ vuecheckbox/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <check-box></check-box>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import checkBox from "./components/checkBox.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    checkBox,
+  },
+};
 </script>
 
 <style>
---
+++ vuecheckbox/src/components/checkBox.vue
@@ -0,0 +1,56 @@
+<template>
+  <!--  -->
+  <div id="div">
+    <input type="checkbox" id="all" value="all" v-model="checkListAll" />
+    <label for="all">all</label>
+    <div id="list" v-for="check in checkList" :key="check.id">
+      <label :for="checkList.id"
+        ><input
+          type="checkbox"
+          :id="checkList.id"
+          :value="checkList.value"
+          v-model="check.checked"
+      /></label>
+
+      <p>checked: {{ checkList }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "checkBox",
+  data() {
+    return {
+      checkListAll: "",
+      checkList: [
+        {
+          id: "ch1",
+          value: "ch1",
+          checked: false,
+        },
+        {
+          id: "ch2",
+          value: "ch2",
+          checked: false,
+        },
+        {
+          id: "ch3",
+          value: "ch3",
+          checked: false,
+        },
+        {
+          id: "ch4",
+          value: "ch4",
+          checked: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    checkListAllList() {},
+  },
+};
+</script>
+
+<style scoped></style>
