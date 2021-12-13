5674f35b824ec8fdc45315246958bedfc740b694
이수현_이모션 <soohyeon.lee@emotion.co.kr>
2021-12-13 23:09:53 +0900

    중간커미스,,,,,

--- vuecheckbox/src/App.vue
+++ vuecheckbox/src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <check-box @allCheckedList="testEvent"></check-box>
+    <check-box>
+      <input type="checkbox" id="all" value="all" v-model="allChecked" />
+    </check-box>
+    <ul>
+      <li v-for="(item, index) in checkList.list" :key="index">
+        <check-box
+          v-model="checkList.itemCheck"
+          :id="item.id"
+          :name="item.name"
+          :val="item.value"
+        >
+        </check-box
+        >{{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,14 +26,43 @@ export default {
   components: {
     checkBox,
   },
-  data(){
+  props: {},
+  data() {
     return {
-    }
+      allChecked: false, // 전체 체크 falseecked 된 input 담김
+      checkList: {
+        itemCheck: [], // 체크한 값
+        list: [
+          // 데이터
+          {
+            id: "ch1",
+            value: "ch1",
+            name: "ch1",
+          },
+          {
+            id: "ch2",
+            value: "ch2",
+            name: "ch2",
+          },
+          {
+            id: "ch3",
+            value: "ch3",
+            name: "ch3",
+          },
+          {
+            id: "ch4",
+            value: "ch4",
+            name: "ch4",
+          },
+        ],
+      },
+    };
   },
+  computed: {},
   methods: {
-    testEvent(){
-    }
-  }
+    testEvent() {},
+    allCheckedBtn() {},
+  },
 };
 </script>
 
--- vuecheckbox/src/components/checkBox.vue
+++ vuecheckbox/src/components/checkBox.vue
@@ -1,79 +1,47 @@
 <template>
-  <!--  -->
-  <div id="div">
-    <input type="checkbox" id="all" value="all" v-model="allChecked" @click="allCheckedList"/>
-    {{allChecked}}
-<!--    -->
-    <label for="all">all</label>
-    <div id="list" v-for="item in checkList" :key="item.id" >
-      <label :for="item.id" @click="checkedItem"
-        ><input
-          type="checkbox"
-          :id="item.id"
-          :value="item.value"
-          v-model="item.checked"
-        />{{ item.name }}
-      </label>
-
-      <p>checked: {{ item.checked }}</p>
-    </div>
-  </div>
+  <label>
+    <input type="checkbox" ref="checkBox" />
+  </label>
+  <!--  TODO 질문 list
+        $attrs: class ,style를 포함해서 전달되는 모든 속성을 의미함 ( 컴포넌트에 전달된 속성 )
+         대체로 부모> 자식으로 보낸다는 글을 봤는데 html로 뿌려지는걸 생각하면 되는지,,,, 두루뭉실한이해,,,,-->
+  <!--  :checked="checked.some((el) => el === val)" 체크 된 애들 확인 다시하는거? -->
+  <!--  $listeners :컴포넌트에서 사용된 이벤트 리스너를 포함하는 오브젝트/ 부모> 자식 /.n개의 value나 event를 넘기는데 사용합니다
+        구성 요소가 내보내는 이벤트를 수신하는 구성 요소가 있으면 채워집니다. (?)-->
 </template>
 
 <script>
 export default {
   name: "checkBox",
-  //prop:['checked', 'item'],
+  props: {},
   data() {
-    return {
-      allChecked: false,
-      checkList: [
-        {
-          id: "ch1",
-          value: "ch1",
-          name:'ch1',
-          checked: false,
-        },
-        {
-          id: "ch2",
-          value: "ch2",
-          name:'ch2',
-          checked: false,
-        },
-        {
-          id: "ch3",
-          value: "ch3",
-          name:'ch3',
-          checked: false,
-        },
-        {
-          id: "ch4",
-          value: "ch4",
-          name:'ch4',
-          checked: false,
-        },
-      ],
-    };
+    return {};
   },
-  methods: {
-    checkedItem(){
-      this.checkList.checked = true;
-      console.log(this.checkList)
-    },
-    allCheckedList() {
-
-    },
+  computed: {
+    /* inputListeners: function () {
+      return {
+        ...this.$listeners,
+        change: (event) => {
+          this.$emit("change", event.target.checked);
+          console.log(event.target);
+        }, //$listeners 모아서 합칠거야? > 근데 왜?
+      };
+      // `Object.assign` 는 오브젝트를 새로운 오브젝트로 병합합니다.
+    },*/
   },
+  methods: {},
 };
 </script>
 
 <style scoped>
-label{
-  outline: 1px solid #000;
-}
-input{
-  background:Red;
+label {
+  position: relative;
+  display: inline-block;
+  padding: 5px 0 5px 30px;
+  align-items: center;
+  cursor: pointer;
 }
 
-
+input {
+}
 </style>
