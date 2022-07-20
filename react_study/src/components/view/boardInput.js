97dc40d0adb2b3dd8e10a28064379820de692224
이수현 <hyeondex@gmail.com>
2022-07-20 18:46:01 +0900

    study

---
+++ react_study/src/components/view/boardInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BoardInput(props) {
+    return (
+        <div>
+            <input type="text"/>
+        </div>
+    );
+}
+
+export default BoardInput;
---
+++ react_study/src/components/view/boardList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BoardList(props) {
+    return (
+        <div>
+            {props.name}
+            <li></li>
+        </div>
+    );
+}
+
+export default BoardList;
