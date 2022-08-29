404ea95af7d834341c82ecd9f99931656cabd615
이수현 <hyeondex@gmail.com>
2022-08-29 15:51:11 +0900

    중간 커밋

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -3,40 +3,59 @@ import ExpenseItem from "./components/ExpenseItem";
 
 // app
 function App() {
-    const data = [
-        {
-            id : 0,
-            title : "title1",
-            amount : "amount1",
-            date : new Date(2022,8,17)
-        },
-        {
-            id : 1,
-            title : "title2",
-            amount : "amount2",
-            date : new Date(2022,8,17)
-        },
-        {
-            id : 2,
-            title : "title3",
-            amount : "amount3",
-            date : new Date(2022,8,17)
-        },
-        {
-            id : 3,
-            title : "title4",
-            amount : "amount4",
-            date : new Date(2022,8,17)
-        }
-    ]
+  const data = [
+    {
+      id: 0,
+      title: "title1",
+      amount: 9800,
+      date: new Date(2022, 2, 17),
+    },
+    {
+      id: 1,
+      title: "title2",
+      amount: 10000,
+      date: new Date(2022, 0, 17),
+    },
+    {
+      id: 2,
+      title: "title3",
+      amount: 900,
+      date: new Date(2022, 8, 17),
+    },
+    {
+      id: 3,
+      title: "title4",
+      amount: 5000000,
+      date: new Date(2022, 8, 17),
+    },
+  ];
 
   return (
     <div>
-        <ExpenseItem title={data[0].title} id={data[0].id} amount={data[0].amount} date={data[0].date} ></ExpenseItem>
-        <ExpenseItem title={data[1].title} id={data[1].id} amount={data[1].amount} date={data[1].date} ></ExpenseItem>
-        <ExpenseItem title={data[2].title} id={data[2].id} amount={data[2].amount} date={data[2].date} ></ExpenseItem>
-        <ExpenseItem title={data[3].title} id={data[3].id} amount={data[3].amount} date={data[3].date} ></ExpenseItem>
-
+      <ExpenseItem
+        title={data[0].title}
+        id={data[0].id}
+        amount={data[0].amount}
+        date={data[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={data[1].title}
+        id={data[1].id}
+        amount={data[1].amount}
+        date={data[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={data[2].title}
+        id={data[2].id}
+        amount={data[2].amount}
+        date={data[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={data[3].title}
+        id={data[3].id}
+        amount={data[3].amount}
+        date={data[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- react_study/src/components/ExpenseItem.js
+++ react_study/src/components/ExpenseItem.js
@@ -2,15 +2,23 @@ import "./ExpenseItem.css";
 
 // 비용 관련 component
 function ExpenseItem(props) {
-
+  // 위에 변수로 빼서 사용하기
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "numeric" });
   return (
     <div className="expense-item">
-        <div>{props.id}</div>
-      <div >{props.title}</div>
-        <h2 >{props.date.toDateString()}</h2>
-      <div >
-        <div>{props.amount}</div>
+      <div className="number">{props.id}</div>
+      <div className="desc">
+        {props.title}
+        <div className="date">
+          <div>{month}</div>
+          <div>{day}</div>
+          <div>{year}</div>
+        </div>
       </div>
+
+      <div className="price">{props.amount}</div>
     </div>
   );
 }
