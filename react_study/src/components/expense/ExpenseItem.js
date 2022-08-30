ff60b3e696cb3e2152cf9d799229b432b1a7d240
이수현 <hyeondex@gmail.com>
2022-08-30 18:53:51 +0900

    useState modify

--- react_study/src/components/expense/ExpenseItem.js
+++ react_study/src/components/expense/ExpenseItem.js
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
-import "./ExpenseDate";
 import ExpenseDate from "./ExpenseDate";
 import ExpensePrice from "./ExpensePrice";
 
 // 비용 관련 component
 const ExpenseItem = (props) => {
   // 위에 변수로 빼서 사용하기
+
+  //let title = props.title;
+  const [title, updateTitle] = useState(props.title);
+  const clickHandler = () => {
+    console.log("실행");
+    updateTitle("updated! title");
+  };
+
   return (
     <div className="expense-item">
       <div className="number">{props.id}</div>
       <div className="desc">
-        {props.title}
+        {title}
         <ExpenseDate date={props.date} />
       </div>
       <ExpensePrice price={props.amount} />
+      <button onClick={clickHandler}>button</button>
     </div>
   );
 };
