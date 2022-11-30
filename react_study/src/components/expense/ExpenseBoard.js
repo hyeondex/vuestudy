dfdeb0b31b773b7ad12f50585ca9891d1c4ce945
이수현 <hyeondex@gmail.com>
2022-11-30 18:59:28 +0900

    페이지 새로 로딩되었을때 리스트 뜨지 않는 이슈 해결 안됨

--- react_study/src/components/expense/ExpenseBoard.js
+++ react_study/src/components/expense/ExpenseBoard.js
@@ -2,13 +2,18 @@ import "./ExpenseBoard.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../common/Card";
 import ExpenseFilter from "../newExpense/ExpenseFilter";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //import { useEffect } from "react";
 
 const ExpenseBoard = (props) => {
   const [temp, setTemp] = useState([]);
   const [year, setYear] = useState("2022");
-  // dataHandler 실행 >
+  /*  if (year === "2022") {
+    console.log("start");
+    const defaultYearList = props.data.filter((data) => data.date === 2022);
+    console.log(defaultYearList);
+    return setTemp(defaultYearList);
+  }*/
   const dateHandler = (val) => {
     const tempList = props.data.filter((data) => {
       const tempYear = new Date(data.date).toLocaleDateString("en-us", {
@@ -29,7 +34,7 @@ const ExpenseBoard = (props) => {
         {temp.map((data, idx) => (
           <ExpenseItem
             key={idx}
-            id={data.id === 0 ? "" : data.id}
+            id={data.id}
             title={data.title}
             amount={data.amount}
             date={data.date}
