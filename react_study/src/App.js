d71b06319c6795bbd59dbae9be7f9abe8f42cb15
이수현 <hyeondex@gmail.com>
2022-12-01 15:37:31 +0900

    css 중간 수정

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,43 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseBoard from "./components/expense/ExpenseBoard";
 import NewExpense from "./components/newExpense/NewExpense";
-
+import "../src/asset/common.css";
+import Card from "./components/common/Card";
 // app
+
+const Dummy_data = [
+  {
+    id: 0,
+    title: "title1",
+    amount: 9800,
+    date: new Date(2022, 2, 17),
+  },
+  {
+    id: 1,
+    title: "title2",
+    amount: 10000,
+    date: new Date(2019, 0, 17),
+  },
+  {
+    id: 2,
+    title: "title3",
+    amount: 900,
+    date: new Date(2021, 8, 17),
+  },
+  {
+    id: 3,
+    title: "title4",
+    amount: 5000000,
+    date: new Date(2022, 8, 17),
+  },
+];
 const App = () => {
-  const data = [
-    {
-      id: 0,
-      title: "title1",
-      amount: 9800,
-      date: new Date(2022, 2, 17),
-    },
-    {
-      id: 1,
-      title: "title2",
-      amount: 10000,
-      date: new Date(2019, 0, 17),
-    },
-    {
-      id: 2,
-      title: "title3",
-      amount: 900,
-      date: new Date(2018, 8, 17),
-    },
-    {
-      id: 3,
-      title: "title4",
-      amount: 5000000,
-      date: new Date(2022, 8, 17),
-    },
-  ];
+  const [data, setData] = useState(Dummy_data);
   const addExpenseHandler = (expense) => {
-    console.log("props를 받아 app으로");
-    console.log(expense);
+    //console.log("props를 받아 app으로");
+    setData((prevState) => {
+      return [expense, ...prevState];
+    }); //기존 데이터 + 새롭게 form에서 추가 되는 데이터
   };
   return (
-    <div>
-      <NewExpense onExpenseHandler={addExpenseHandler} />
-      <ExpenseBoard data={data} />
+    <div className="container">
+      <Card>
+        <NewExpense onExpenseHandler={addExpenseHandler} />
+        <ExpenseBoard data={data} />
+      </Card>
     </div>
   );
 };
