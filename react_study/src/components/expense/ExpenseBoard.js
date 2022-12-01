c1a85e2735c065c8114d3b6b75888e6f801b3ccd
이수현 <hyeondex@gmail.com>
2022-12-01 17:18:40 +0900

    nodata case add

--- react_study/src/components/expense/ExpenseBoard.js
+++ react_study/src/components/expense/ExpenseBoard.js
@@ -2,37 +2,29 @@ import "./ExpenseBoard.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../common/Card";
 import ExpenseFilter from "../newExpense/ExpenseFilter";
-import { useEffect, useState } from "react";
-//import { useEffect } from "react";
+import { useState } from "react";
 
 const ExpenseBoard = (props) => {
-  const [temp, setTemp] = useState([]);
   const [year, setYear] = useState("2022");
-  /*  if (year === "2022") {
-    console.log("start");
-    const defaultYearList = props.data.filter((data) => data.date === 2022);
-    console.log(defaultYearList);
-    return setTemp(defaultYearList);
-  }*/
   const dateHandler = (selectedYear) => {
-    const tempList = props.data.filter((data) => {
-      const tempYear = new Date(data.date).toLocaleDateString("en-us", {
-        year: "numeric",
-      });
-      if (selectedYear === tempYear) {
-        return data;
-      }
-    });
-    setTemp(tempList);
     setYear(selectedYear);
   };
+
+  const tempList = props.data.filter((data) => {
+    return data.date.getFullYear().toString() === year;
+  });
+
+  //nodata case
+  if (tempList.length === 0) {
+    return <div className="nodata">nodata</div>;
+  }
 
   return (
     <Card>
       <ExpenseFilter onDateHandler={dateHandler} selected={year} />
-      {temp.map((data, idx) => (
+      {tempList.map((data) => (
         <ExpenseItem
-          key={idx}
+          key={data.id}
           title={data.title}
           amount={data.amount}
           date={data.date}
